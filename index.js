f4bed5c3abcc98b5298e4b418c25369f450f7a59
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 07:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(kitten) {
-  return kittens.push (kitten)
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten)
 }
